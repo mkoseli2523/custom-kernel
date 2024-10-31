4a2573b4621bcbe34339e2cfb58aa1ff5c1715c5
Ronnoc0913 <cctan2@illinois.edu>
2024-10-31 14:02:21 -0500

    moved structs and relevant defines to the elf.h

--- src/kern/elf.c
+++ src/kern/elf.c
@@ -3,56 +3,6 @@
 #include <stdint.h>
 #include <string.h> 
 
-//           Relevant defines
-#define EI_NIDENT   16
-#define ELFMAG0     0x7f
-#define ELFMAG1     'E'
-#define ELFMAG2     'L'
-#define ELFMAG3     'F'
-#define PT_LOAD     1  // Program header type for loadable segments
-
-#define RV64_MACHINE 243
-
-// Validate that file is in ELF format
-#define ELF_MAGIC_OK(ehdr) ((ehdr).e_ident[0] == ELFMAG0 && \
-                            (ehdr).e_ident[1] == ELFMAG1 && \
-                            (ehdr).e_ident[2] == ELFMAG2 && \
-                            (ehdr).e_ident[3] == ELFMAG3)
-
-#define LOAD_START  0x80100000
-#define LOAD_END    0x81000000
-
-// Elf Header Structure for 64 bit ELF
-typedef struct elf64_hdr
-{
-    unsigned char   e_ident[EI_NIDENT];     // ELF identification bytes
-    uint16_t        e_type;                 // Object file type
-    uint16_t        e_machine;              // Architecture
-    uint32_t        e_version;              // ELF Version
-    uint64_t        e_entry;                // Entry point address
-    uint64_t        e_phoff;                // Program header table offset
-    uint64_t        e_shoff;                // Section header table offset
-    uint32_t        e_flags;                // Processor specific flags
-    uint16_t        e_ehsize;               // ELF header size
-    uint16_t        e_phentsize;            // Program Header entry size
-    uint16_t        e_phnum;
-    uint16_t        e_shentsize;
-    uint16_t        e_shnum;
-    uint16_t        e_shstrndx;
-} Elf64_Ehdr;
-
-// Program Header Strucutre for 64 bit ELF
-typedef struct elf64_phdr{
-    uint32_t        p_type;
-    uint32_t        p_flags;
-    uint64_t        p_offset;
-    uint64_t        p_vaddr;
-    uint64_t        p_paddr;
-    uint64_t        p_filesz;
-    uint64_t        p_memsz;
-    uint64_t        p_align;
-} Elf64_Phdr;
-
 int elf_load(struct io_intf *io, void (**entryptr)(struct io_intf *io)){
     Elf64_Ehdr elf_header;
 
